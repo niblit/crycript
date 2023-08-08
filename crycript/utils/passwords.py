e262c9068eb59b47a782a7d375142cd796bf31d2
niblit <niblit.gh@gmail.com>
2023-08-08 13:49:10 -0600

    Removed useless salt generation function

--- crycript/utils/passwords.py
+++ crycript/utils/passwords.py
@@ -1,6 +1,7 @@
 from base64 import urlsafe_b64encode
 from getpass import getpass
 from hashlib import sha3_512
+from os import urandom
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.hashes import SHA3_512
@@ -8,44 +9,14 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 from crycript import constants, exceptions
 
-
-def lfsr_salt(password) -> bytes:
-    binary = bin(
-        int(
-            sha3_512(
-                password.encode()
-            ).hexdigest(),
-            16
-        )
-    )[-128:]
-
-    salt = sha3_512()
-
-    def str_xor(a: str, b: str) -> str:
-        return '0' if a == b else '1'
-
-    for _ in range(constants.SALT_ITERATIONS):
-        num = ""
-        for _ in range(8):
-            xor = str_xor(binary[-1], binary[-2])
-            xor = str_xor(xor, binary[-3])
-            xor = str_xor(xor, binary[-8])
-
-            binary = xor + binary[:-1]
-            num += binary[-1]
-
-        salt.update(
-            chr(
-                int(num, 2)
-            ).encode()
-        )
-    return salt.hexdigest().encode()
-
+def generate_salt():
+    return urlsafe_b64encode(urandom(32))
 
 def get_key(
         confirm_password: bool = True,
         password_message: str = 'Password: ',
-        confirmation_message: str = 'Repeat Password: '
+        confirmation_message: str = 'Repeat Password: ',
+        salt: str = None
 ) -> bytes:
     try:
         password = getpass(password_message)
@@ -92,15 +63,18 @@ def get_key(
             print()
             raise SystemExit
 
+    if salt is None:
+        salt = generate_salt()
+
     kdf = PBKDF2HMAC(
         algorithm=SHA3_512(),
         length=32,
-        salt=lfsr_salt(password),
+        salt=salt,
         iterations=constants.PBKDF2_ITERATIONS,
         backend=default_backend()
     )
 
-    return urlsafe_b64encode(
+    return salt, urlsafe_b64encode(
         kdf.derive(
             password.encode()
         )
